cfa9a7e2068a8a28f79b8b8f71453d0e1ae411af
Drew Finnis <drewfinnis@gmail.com>
2021-05-26 16:13:59 +0200

    feature/empty main and src/ runs

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	forest "Random_Forest/src"
+)
+
+func main() {
+	forest.RandomForest()
+}
+
+// ## To run enter either command:
+// go run main.go
+// go build; ./Random_Forest
---
+++ src/RandomForest.go
@@ -0,0 +1,8 @@
+package forest
+
+import "fmt"
+
+// RandomForest is the main & only exposed function
+func RandomForest() {
+	fmt.Printf("Oh Hi!!\n") ///////////////////
+}
