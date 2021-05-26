cb084a3a0828e9306caef80e415f09153b0126ec
Drew Finnis <drewfinnis@gmail.com>
2021-05-26 18:29:30 +0200

    feature/basic tree struct

--- src/RandomForest.go
+++ src/RandomForest.go
@@ -2,6 +2,32 @@ package forest
 
 import "fmt"
 
+type node struct {
+	feature   uint8
+	split     float32
+	diagnosis bool
+	childA    *node
+	childB    *node
+}
+
+type forest struct {
+	// depth uint
+	trees []node
+}
+
+// // newNode initializes a node
+// func newNode(forest forest) forest {
+// 	node := node{}
+// 	forest.trees = append(forest.trees, node)
+// 	return forest
+// }
+
+func initForest() forest {
+	forest := forest{}
+	forest.trees = append(forest.trees, node{})
+	return forest
+}
+
 // RandomForest is the main & only exposed function
 func RandomForest() {
 	// flags := parseArg()
@@ -11,8 +37,12 @@ func RandomForest() {
 	// data := preprocess(flags.dataPath)
 	data := preprocess("data.csv")
 	train_set, test_set := splitData(data /*, flags*/)
-
 	fmt.Printf("len(train_set): %v\n", len(train_set)) ///////////////////
 	fmt.Printf("len(test_set): %v\n", len(test_set))   ///////////////////
-	fmt.Printf("Oh Hi!!\n")                            ///////////////////
+
+	// Initialize
+	forest := initForest()
+	fmt.Printf("forest: %v\n", forest) ///////////////////
+
+	fmt.Printf("Oh Hi!!\n") ///////////////////
 }
