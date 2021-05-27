c3df6348bb4b68277bb9ccb7b3366d5d82a9c797
Drew Finnis <drewfinnis@gmail.com>
2021-05-27 15:07:26 +0200

    feature/WIP giniImpurity

--- src/RandomForest.go
+++ src/RandomForest.go
@@ -1,17 +1,21 @@
 package forest
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type node struct {
-	feature   uint8
-	split     float32
-	diagnosis bool
-	childA    *node
-	childB    *node
+	feature    uint8 // data column
+	split      float32
+	childLeft  *node
+	childRight *node
+	// diagnosis  bool // above or below split = Malignant
 }
 
 type forest struct {
 	// depth uint
+	// size  uint
 	trees []node
 }
 
@@ -24,8 +28,30 @@ type forest struct {
 
 func initForest() forest {
 	forest := forest{}
-	forest.trees = append(forest.trees, node{})
+	forest.trees = append(forest.trees, node{}) // root
 	return forest
+}
+
+func sortSet(set [][]float32, feature uint) [][]float32 {
+	sort.SliceStable(set, func(i, j int) bool {
+		return set[i][feature] < set[j][feature]
+	})
+	return set
+}
+
+func giniImpurity(set [][]float32, feature uint, split float32) float32 {
+	var impurity float32
+	set = sortSet(set, feature)
+	for i, sample := range set {
+		fmt.Printf("%3v. sample[feature]: %-13v, diagnosis: %v\n", i, sample[feature], sample[0])
+	}
+	return impurity
+}
+
+func train(forest forest, train_set [][]float32) {
+	fmt.Printf("\n%v%vTrain Forest%v\n\n", BOLD, UNDERLINE, RESET)
+	impurity := giniImpurity(train_set, 1, 0)
+	fmt.Printf("impurity: %v\n\n", impurity)
 }
 
 // RandomForest is the main & only exposed function
@@ -34,7 +60,6 @@ func RandomForest() {
 	printHeader(flags)
 
 	// Data
-	// data := preprocess(flags.dataPath)
 	data := preprocess(flags.dataPath)
 	train_set, test_set := splitData(data /*, flags*/)
 	fmt.Printf("len(train_set): %v\n", len(train_set)) ///////////////////
@@ -42,7 +67,12 @@ func RandomForest() {
 
 	// Initialize
 	forest := initForest()
-	fmt.Printf("forest: %v\n", forest) ///////////////////
+	// fmt.Printf("forest: %v\n", forest) ///////////////////
+
+	// Train
+	train(forest, train_set)
+
+	// Predict
 
 	fmt.Printf("Oh Hi!!\n") ///////////////////
 }
