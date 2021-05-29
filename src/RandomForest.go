b2e6536757f50845c750e25df0b811fdda6ba1ec
Drew Finnis <drewfinnis@gmail.com>
2021-05-29 13:56:28 +0200

    refacto/cleanup print statements

--- src/RandomForest.go
+++ src/RandomForest.go
@@ -1,9 +1,5 @@
 package forest
 
-import (
-	"fmt"
-)
-
 // RandomForest is the main & only exposed function
 func RandomForest() {
 	flags := parseArg()
@@ -12,8 +8,6 @@ func RandomForest() {
 	// Data
 	data := preprocess(flags.dataPath)
 	train_set, test_set := splitData(data /*, flags*/)
-	fmt.Printf("len(train_set): %v\n", len(train_set)) ///////////////////
-	fmt.Printf("len(test_set): %v\n", len(test_set))   ///////////////////
 
 	// Initialize
 	forest := initForest()
@@ -22,8 +16,7 @@ func RandomForest() {
 	train(forest, train_set, test_set, flags)
 
 	// Predict
-	predict(forest, test_set)
+	// predict(forest, test_set)
 
 	// printTree(&forest.trees[0], 0)
-	fmt.Printf("Oh Hi!!\n") ///////////////////
 }
