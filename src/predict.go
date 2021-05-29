c45bb0e11fdf0ff167736eae000d899ec49ded56
Drew Finnis <drewfinnis@gmail.com>
2021-05-29 11:56:43 +0200

    refacto/cleanup comments

--- src/predict.go
+++ src/predict.go
@@ -30,17 +30,11 @@ func truthTally(predictions, truth []bool) (tp, fn, fp, tn uint) {
 func predictTally(forest forest, data [][]float32) (tp, fn, fp, tn uint) {
 	var predictions []bool
 	var truth []bool
-	// for sample := 0; sample < len(test_set); sample++ {
 
 	tree := forest.trees[0]
 	// printNode(&tree, 0)
 
 	for _, sample := range data {
-		// fmt.Printf("sample %v: %v\n", i, sample[0])
-		// if sample[0] == 1 { // Malignant
-		// 	// fmt.Printf("M\n")
-		// }
-		// truth := append(truth, bool(sample[0]))
 		node := tree
 		for {
 			// index := [31]string{"None (leaf)", "Radius Mean", "Texture Mean", "Perimeter Mean", "Area Mean", "Smoothness Mean", "Compactness Mean", "Concavity Mean", "Concave points Mean", "Symmetry Mean", "Fractal dimension Mean", "Radius se", "Texture se", "Perimeter se", "Area se", "Smoothness se", "Compactness se", "Concavity se", "Concave points se", "Symmetry se", "Fractal dimension se", "Radius Worst", "Texture Worst", "Perimeter Worst", "Area Worst", "Smoothness Worst", "Compactness Worst", "Concavity Worst", "Concave points Worst", "Symmetry Worst", "Fractal dimension Worst"} // data.csv column titles
@@ -58,8 +52,7 @@ func predictTally(forest forest, data [][]float32) (tp, fn, fp, tn uint) {
 				// printNode(&node, node.depth) ////////////
 				// prediction := node.diagnosis
 				predictions = append(predictions, node.diagnosis)
-				// fmt.Printf("prediction: %v\n", prediction) //////////////
-				// truth := sample[0]
+
 				if sample[0] == 1 { // Malignant
 					truth = append(truth, true)
 				} else {
@@ -75,7 +68,6 @@ func predictTally(forest forest, data [][]float32) (tp, fn, fp, tn uint) {
 	// for i := 0; i < len(predictions); i++ {
 	// 	fmt.Printf("%-3v prediction: %-5v, truth: %v\n", i, predictions[i], truth[i])
 	// }
-	// tp, fn, fp, tn := truthTally(predictions, truth)
 	return truthTally(predictions, truth)
 }
 
