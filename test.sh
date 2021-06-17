2df4e183d04c84c70a0f4242fafa7d25b4347d00
Drew Finnis <drewfinnis@gmail.com>
2021-06-17 11:33:42 +0200

    feature/WIP test accuracy over depth

--- test.sh
+++ test.sh
@@ -1,1 +1,20 @@
-go run main.go
+#### -- Random Forest Test -- ####
+go build
+
+#### -- Print Header -- ####
+RESET="\x1b[0m"
+BOLD="\x1b[1m"
+ITALIC="\x1b[3m"
+
+printf "\E[H\E[2J" ## Clear screen
+printf $BOLD
+printf $ITALIC
+echo "Launching Random Forest Test...$RESET\n"
+
+cmd="./Random_Forest"
+output=$(eval "$cmd")
+accuracy=$(echo "$output" | grep Accuracy)
+# echo $output
+echo $accuracy
+
+rm Random_Forest
