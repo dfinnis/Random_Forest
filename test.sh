c88d89305eb8633fcb49ab522e85adbcc931240b
Drew Finnis <drewfinnis@gmail.com>
2021-06-17 12:32:51 +0200

    feature/unit test finds mean accuracy for depth 1-9

--- test.sh
+++ test.sh
@@ -12,6 +12,10 @@ printf $ITALIC
 echo "Launching Random Forest Test...$RESET\n"
 
 
+#### -- Config -- ####
+CASES=10
+
+
 #### -- Test Function -- ####
 unit_test()
 {
@@ -19,25 +23,43 @@ unit_test()
 	# echo "Oh hi!" ##########
 	echo $DEPTH
 	cmd="./Random_Forest -d $DEPTH"
-	output=$(eval "$cmd")
-	accuracyTrain=$(echo "$output" | grep Accuracy | cut -d "|" -f 3)
-	accuracyTest=$(echo "$output" | grep Accuracy | cut -d "|" -f 4)
-	# accuracy=$(echo "$output" | grep Accuracy) ##########
-	# echo $output ##########
-	# echo $accuracy ##########
-	echo $accuracyTrain
-	echo $accuracyTest
+	accuracyTrainTotal=0
+	accuracyTestTotal=0
+
+	test=0
+	while [ $test -lt $CASES ]
+	do
+		# echo $test
+		output=$(eval "$cmd")
+		accuracyTrain=$(echo "$output" | grep Accuracy | cut -d "|" -f 3)
+		accuracyTest=$(echo "$output" | grep Accuracy | cut -d "|" -f 4)
+		# accuracy=$(echo "$output" | grep Accuracy) ##########
+		# echo $output ##########
+		# echo $accuracy ##########
+		accuracyTrainTotal=$(echo "$accuracyTrainTotal + $accuracyTrain" | bc)
+		accuracyTestTotal=$(echo "$accuracyTestTotal + $accuracyTest" | bc)
+		# echo $accuracyTrain
+		# echo $accuracyTrainTotal
+
+		# echo $accuracyTest
+		test=$(($test + 1))
+	done
+	accuracyTrainMean=$(echo "scale = 7; $accuracyTrainTotal / $CASES" | bc)
+	accuracyTestMean=$(echo "scale = 7; $accuracyTestTotal / $CASES" | bc)
+	echo $accuracyTrainMean
+	echo $accuracyTestMean
 	echo ##########
 }
 
+# unit_test 1
 
-unit_test 1
-
+#### -- Depth -- ####
 depth=1
 while [ $depth -lt 10 ]
 do
-	depth=$(($depth + 1))
 	unit_test $depth
+	depth=$(($depth + 1))
 done
 
+#### -- Cleanup -- ####
 rm Random_Forest
